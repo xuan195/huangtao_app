83587d1bcb311e303c155cce7be094b4060cbbb7
xuan195 <xuan195@126.com>
2021-02-03 11:11:27 +0800

    APP安装文件

--- test/back.dart
+++ test/back.dart
@@ -13,7 +13,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -35,55 +34,56 @@ class RedditSlider extends StatefulWidget {
 }
 
 class _RedditSliderState extends State<RedditSlider> {
-  final PageController ctrl= PageController(viewportFraction: 0.80);
+  final PageController ctrl = PageController(viewportFraction: 0.80);
   Stream slides;
   int currentPage = 0;
   String activeSubreddit = 'earthporn';
   List<RedditPost> slideList = [];
 
-  _fetchImages() async{
+  _fetchImages() async {
     List<RedditPost> list = [];
-    final response = await http.get('https://www.reddit.com/r/$activeSubreddit/.json?limit=15');
-    if(response.statusCode == 200){
+    final response = await http
+        .get('https://www.reddit.com/r/$activeSubreddit/.json?limit=15');
+    if (response.statusCode == 200) {
       final data = json.decode(response.body);
-      for(Map d in data['data']['children']){
-        if(d['data']['url'].toString().contains('jpg')){
+      for (Map d in data['data']['children']) {
+        if (d['data']['url'].toString().contains('jpg')) {
           RedditPost post = RedditPost.fromJson(d['data']);
           list.add(post);
         }
       }
-    }else {
+    } else {
       throw Exception("Failed to load reddit.");
     }
-  setState(() {
-    slideList = list;
-  });
+    setState(() {
+      slideList = list;
+    });
   }
 
-   @override
-   void initState(){
-     _fetchImages();
-     ctrl.addListener(() {
-       int next = ctrl.page.round();
-       if(currentPage != next){
-         setState(() {
-           currentPage = next;
-         });
-       }
-     });
-     super.initState();
-   }
+  @override
+  void initState() {
+    _fetchImages();
+    ctrl.addListener(() {
+      int next = ctrl.page.round();
+      if (currentPage != next) {
+        setState(() {
+          currentPage = next;
+        });
+      }
+    });
+    super.initState();
+  }
 
-  _buildButton(subreddit){
+  _buildButton(subreddit) {
     Color color = subreddit == activeSubreddit
         ? Theme.of(context).primaryColor
         : Colors.white;
     Color textColor = !(subreddit == activeSubreddit)
         ? Theme.of(context).primaryColor
-        : Colors.white;    
+        : Colors.white;
     return RaisedButton(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18.0)),
-      onPressed: (){
+      onPressed: () {
         setState(() {
           activeSubreddit = subreddit;
         });
@@ -91,93 +91,91 @@ class _RedditSliderState extends State<RedditSlider> {
       color: color,
       child: Text(
         '/r/$subreddit',
-        style: TextStyle(color:textColor),
+        style: TextStyle(color: textColor),
       ),
     );
   }
-  _buildSubredditPage(){
+
+  _buildSubredditPage() {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[        
-        Text('SubReddit',style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),),
-        Text('Choose',style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+      children: <Widget>[
+        Text(
+          'SubReddit',
+          style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+        ),
+        Text(
+          'Choose',
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+        ),
         _buildButton('earthporn'),
         _buildButton('foodporn'),
         _buildButton('nocontextpics'),
-
       ],
     );
   }
 
-  _buildPicPage(RedditPost post,active){
+  _buildPicPage(RedditPost post, active) {
     final double topMargin = active ? 100 : 200;
-    final double offset = active ? 20 :0;
+    final double offset = active ? 20 : 0;
     final double blur = active ? 100 : 200;
 
     return AnimatedContainer(
       duration: Duration(milliseconds: 500),
       curve: Curves.easeInOutQuint,
-      margin: EdgeInsets.only(top: topMargin, bottom:20, right: 15),
+      margin: EdgeInsets.only(top: topMargin, bottom: 20, right: 15),
       child: Column(
         children: <Widget>[
           Container(
             height: 60.0,
             child: Center(
-              child: Text(
-                post.title,
-                textAlign:TextAlign.center,
-                style: TextStyle(fontSize:15.0,color: Colors.white),
-              )
-            ),
+                child: Text(
+              post.title,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 15.0, color: Colors.white),
+            )),
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft:Radius.circular(20.0),
-                topRight: Radius.circular(20.0)),
-              color: Colors.black.withAlpha(100)
-            ),
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20.0),
+                    topRight: Radius.circular(20.0)),
+                color: Colors.black.withAlpha(100)),
           )
         ],
       ),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(20.0)),
-        color:Colors.blue,
-        image: DecorationImage(image: NetworkImage(post.imgUrl),fit: BoxFit.cover),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black87,
-            offset: Offset(offset,offset),
-            blurRadius: blur
-          )
-        ]
-      ),
+          borderRadius: BorderRadius.all(Radius.circular(20.0)),
+          color: Colors.blue,
+          image: DecorationImage(
+              image: NetworkImage(post.imgUrl), fit: BoxFit.cover),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black87,
+                offset: Offset(offset, offset),
+                blurRadius: blur)
+          ]),
     );
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Theme.of(context).primaryColor,
-          Colors.white
-        ],
-        begin: Alignment.topCenter,
-        end: Alignment.bottomCenter)
-      ),
+          gradient: LinearGradient(
+              colors: [Theme.of(context).primaryColor, Colors.white],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter)),
       child: PageView.builder(
-        controller: ctrl,
-        itemCount: slideList.length + 1,
-        itemBuilder:(context, int currentIndex){
-          if(currentIndex == 0){
-            return _buildSubredditPage();
-          }else{
-            bool active = currentIndex == currentPage;
-            return _buildPicPage(slideList[currentIndex - 1],active);
-          }
-        }
-      ),
+          controller: ctrl,
+          itemCount: slideList.length + 1,
+          itemBuilder: (context, int currentIndex) {
+            if (currentIndex == 0) {
+              return _buildSubredditPage();
+            } else {
+              bool active = currentIndex == currentPage;
+              return _buildPicPage(slideList[currentIndex - 1], active);
+            }
+          }),
     );
- 
   }
 }
